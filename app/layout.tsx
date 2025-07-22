58e92659a0a739ab6e5032abb1a2050023315b09
Nikolay Sulteev <Philian73@gmail.com>
2025-07-22 16:52:57 +0300

    feat: main font has been changed to "Nunito"

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,15 @@ import type { ReactNode } from 'react'
 
 import type { Metadata } from 'next'
 
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Nunito } from 'next/font/google'
 
 import '@/app/globals.css'
 import { Providers } from '@/app/providers'
 
-const geistSans = Geist({
-   variable: '--font-geist-sans',
-   subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-   variable: '--font-geist-mono',
-   subsets: ['latin'],
+const nunito = Nunito({
+   variable: '--font-nunito',
+   subsets: ['cyrillic'],
+   weight: ['400', '500', '600', '700', '800', '900'],
 })
 
 export const metadata: Metadata = {
@@ -31,8 +27,7 @@ export default function RootLayout({
       <html lang={'en'} suppressHydrationWarning>
          <body
             className={`
-               ${geistSans.variable}
-               ${geistMono.variable}
+               ${nunito.variable}
                antialiased
             `}
          >
