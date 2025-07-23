cd4d10ffab581f9faeb9eadf12f5b5b7b25ad409
Nikolay Sulteev <Philian73@gmail.com>
2025-07-23 23:17:51 +0300

    feat: removed "eslint-plugin-turbo" as there is no need for a monorepo

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import next from '@next/eslint-plugin-next'
 import baseConfig from '@philian73/eslint-config'
 import betterTailwind from 'eslint-plugin-better-tailwindcss'
-import turbo from 'eslint-plugin-turbo'
 
 export default [
    ...baseConfig,
@@ -9,14 +8,11 @@ export default [
       plugins: {
          '@next/next': next,
          'better-tailwindcss': betterTailwind,
-         turbo,
       },
       rules: {
          ...next.configs.recommended.rules,
          ...betterTailwind.configs['recommended-warn'].rules,
          ...betterTailwind.configs['recommended-error'].rules,
-         ...turbo.configs.recommended.rules,
-         'turbo/no-undeclared-env-vars': 'error',
          'better-tailwindcss/sort-classes': 'warn',
          'better-tailwindcss/enforce-consistent-line-wrapping': [
             'warn',
