9f09c80971996650032197677f5a60b0933e14f5
Nikolay Sulteev <Philian73@gmail.com>
2025-07-26 14:13:56 +0300

    feat: added shared ui component - container

---
+++ src/shared/ui/container.tsx
@@ -0,0 +1,15 @@
+import type { ComponentProps } from 'react'
+
+import { Slot } from '@radix-ui/react-slot'
+
+import { cn } from '@/shared/lib/utils'
+
+type ContainerProps = ComponentProps<'div'> & {
+   asChild?: boolean
+}
+
+export const Container = ({ asChild, className, ...rest }: ContainerProps) => {
+   const Component = asChild ? Slot : 'div'
+
+   return <Component className={cn(`container mx-auto max-w-320 px-4`, className)} {...rest} />
+}
