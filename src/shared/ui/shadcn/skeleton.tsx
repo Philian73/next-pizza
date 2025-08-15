29a794f428cbde6faadaa4e9760c8e9e5120da1a
Nikolay Sulteev <Philian73@gmail.com>
2025-08-15 17:54:44 +0300

    feat: changed bg

--- src/shared/ui/shadcn/skeleton.tsx
+++ src/shared/ui/shadcn/skeleton.tsx
@@ -4,7 +4,13 @@ function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
    return (
       <div
          data-slot={'skeleton'}
-         className={cn('animate-pulse rounded-md bg-accent', className)}
+         className={cn(
+            `
+               animate-pulse rounded-md bg-[oklch(0.967_0.001_286.375)]
+               dark:bg-[oklch(0.274_0.006_286.033)]
+            `,
+            className
+         )}
          {...props}
       />
    )
