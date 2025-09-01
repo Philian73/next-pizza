a27e2a8673e6e656b1b4eb5db0cc3412c9227eec
Nikolay Sulteev <Philian73@gmail.com>
2025-09-01 09:25:19 +0300

    feat: provided nuqs

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,16 +1,20 @@
 import type { ReactNode } from 'react'
 
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+
 import { ThemeProvider } from './theme-provider'
 
 export const Providers = ({ children }: { children: ReactNode }) => {
    return (
-      <ThemeProvider
-         attribute={'class'}
-         defaultTheme={'system'}
-         enableSystem
-         disableTransitionOnChange
-      >
-         {children}
-      </ThemeProvider>
+      <NuqsAdapter>
+         <ThemeProvider
+            attribute={'class'}
+            defaultTheme={'system'}
+            enableSystem
+            disableTransitionOnChange
+         >
+            {children}
+         </ThemeProvider>
+      </NuqsAdapter>
    )
 }
