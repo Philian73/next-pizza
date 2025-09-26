bc5d0d2b5bd75b112e9b9c5e6f95aa9ada80831d
Nikolay Sulteev <Philian73@gmail.com>
2025-09-26 17:34:48 +0300

    feat: remove filters home-page

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,34 +1,25 @@
+import type { SearchParams } from 'nuqs/server'
+
+import { headers } from 'next/headers'
+
 import { Container } from '@/shared/ui/container'
 import { TopBar } from '@/widgets/top-bar'
 
-import { FiltersPanel } from './filters/filters-panel'
-import { FiltersSheet } from './filters/filters-sheet'
 import { Products } from './products'
-import { SortDropdown } from './sort-dropdown'
 
-export const HomePage = () => {
+export const HomePage = async ({ searchParams }: { searchParams: Promise<SearchParams> }) => {
+   const headersList = await headers()
+
+   const pathname = headersList.get('x-pathname') ?? '/'
+
+   console.log(pathname, await searchParams)
+
    return (
       <div className={``}>
          <TopBar />
 
-         <Container className={'flex gap-20 pt-5 pb-10'}>
-            <FiltersPanel className={'max-lg:hidden'} />
-
-            <div className={'flex flex-1 flex-col gap-1.5'}>
-               <div
-                  className={`
-                     flex items-center justify-between gap-1.5
-                     max-sm:flex-col
-                     lg:self-end
-                  `}
-               >
-                  <SortDropdown />
-
-                  <FiltersSheet />
-               </div>
-
-               <Products />
-            </div>
+         <Container className={'pt-4'}>
+            <Products />
          </Container>
       </div>
    )
