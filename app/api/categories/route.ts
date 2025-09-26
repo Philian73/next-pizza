ef1dad88a7863834c58b77003ea0b513d02103b3
Nikolay Sulteev <Philian73@gmail.com>
2025-09-26 17:30:20 +0300

    feat: temp route for categories

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,9 +1,27 @@
+import type { Prisma } from '@prisma/client'
+import type { NextRequest } from 'next/server'
+
 import { NextResponse } from 'next/server'
 
 import { prisma } from '@/shared/lib/prisma'
 
-export async function GET() {
-   const categories = await prisma.category.findMany()
+export async function GET(request: NextRequest) {
+   const onlyWithProducts = request.nextUrl.searchParams.get('onlyWithProducts') === 'true'
 
-   return NextResponse.json(categories)
+   try {
+      const where: Prisma.CategoryWhereInput = {
+         ...(onlyWithProducts && { products: { some: {} } }),
+      }
+
+      const categories = await prisma.category.findMany({ where })
+
+      return NextResponse.json(categories, { status: 200 })
+   } catch (error) {
+      console.error(`[GET /categories]`, error)
+
+      return NextResponse.json(
+         { message: 'Не удалось получить категории', code: 500 },
+         { status: 500 }
+      )
+   }
 }
