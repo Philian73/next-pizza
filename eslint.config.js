2d8ae9b269a076094561f64ecc254c2ebaf43f70
Nikolay Sulteev <Philian73@gmail.com>
2025-10-20 12:08:59 +0300

    feat: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,11 @@
-import next from '@next/eslint-plugin-next'
 import baseConfig from '@philian73/eslint-config'
-import betterTailwind from 'eslint-plugin-better-tailwindcss'
+import next from '@philian73/eslint-config/next'
+import tailwind from '@philian73/eslint-config/tailwind'
 
 export default [
    ...baseConfig,
-   {
-      plugins: {
-         '@next/next': next,
-         'better-tailwindcss': betterTailwind,
-      },
-      rules: {
-         ...next.configs.recommended.rules,
-         ...betterTailwind.configs['recommended-warn'].rules,
-         ...betterTailwind.configs['recommended-error'].rules,
-         'better-tailwindcss/sort-classes': 'warn',
-         'better-tailwindcss/enforce-consistent-line-wrapping': [
-            'warn',
-            {
-               printWidth: 100,
-               lineBreakStyle: 'windows',
-               group: 'newLine',
-               indent: 3,
-            },
-         ],
-         'no-undef': 'off',
-      },
-      settings: {
-         'better-tailwindcss': {
-            entryPoint: 'src/app/globals.css',
-         },
-      },
-   },
+   ...next,
+   ...tailwind,
    {
       files: ['src/shared/lib/prisma/seed/**/*.{js,ts}', 'src/shared/api/schema/generated.ts'],
       rules: {
