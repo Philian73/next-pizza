7ab6417e40e468b6fe2fb43ebc6163112898e6ca
Nikolay Sulteev <Philian73@gmail.com>
2025-10-20 19:15:41 +0300

    feat: sub domains dodo

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       remotePatterns: [
          {
             protocol: 'https',
-            hostname: 'media.dodostatic.net',
+            hostname: '*.dodostatic.net',
          },
       ],
    },
