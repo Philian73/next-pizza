ce1aca16fa1f14d87e64ee6ec8df1619a9b7462e
Nikolay Sulteev <Philian73@gmail.com>
2025-10-21 16:50:37 +0300

    feat: remove unused params

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,19 +1,9 @@
-import type { SearchParams } from 'nuqs/server'
-
-import { headers } from 'next/headers'
-
 import { Container } from '@/shared/ui/container'
 import { TopBar } from '@/widgets/top-bar'
 
 import { Products } from './products'
 
-export const HomePage = async ({ searchParams }: { searchParams: Promise<SearchParams> }) => {
-   const headersList = await headers()
-
-   const pathname = headersList.get('x-pathname') ?? '/'
-
-   console.log(pathname, await searchParams)
-
+export const HomePage = async () => {
    return (
       <div className={``}>
          <TopBar />
