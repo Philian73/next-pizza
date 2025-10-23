7a73f11dab706aee2f866bb8002c23f101e3e4b2
Nikolay Sulteev <Philian73@gmail.com>
2025-10-23 20:46:47 +0300

    feat: used `CartSheet`

--- src/widgets/top-bar/ui/top-bar.tsx
+++ src/widgets/top-bar/ui/top-bar.tsx
@@ -1,12 +1,9 @@
 import type { CategoryItem } from '../model/tob-bar-types'
 
-import { ArrowRightIcon, ShoppingCartIcon } from 'lucide-react'
-
+import { CartSheet } from '@/entities/cart'
 import { categoryApi } from '@/entities/category'
 import { Container } from '@/shared/ui/container'
-import { Button } from '@/shared/ui/shadcn/button'
 import { ScrollArea, ScrollBar } from '@/shared/ui/shadcn/scroll-area'
-import { Separator } from '@/shared/ui/shadcn/separator'
 
 import { CategoriesNav } from './categories-nav'
 
@@ -63,35 +60,7 @@ export const TopBar = async () => {
                <ScrollBar orientation={'horizontal'} className={'-mt-2'} />
             </ScrollArea>
 
-            <Button
-               className={`
-                  group relative gap-4
-                  max-lg:hidden
-               `}
-            >
-               <span className={`font-bold`}>520 ₽</span>
-
-               <Separator className={`opacity-50`} orientation={'vertical'} />
-
-               <div
-                  className={`
-                     flex items-center gap-1 font-bold transition-all duration-300
-                     group-hover:opacity-0
-                  `}
-               >
-                  <ShoppingCartIcon className={'relative'} strokeWidth={2} size={16} />
-
-                  <span className={'font-bold'}>3</span>
-               </div>
-
-               <ArrowRightIcon
-                  size={20}
-                  className={`
-                     absolute right-5 -translate-x-2 opacity-0 transition duration-300
-                     group-hover:translate-x-0 group-hover:opacity-100
-                  `}
-               />
-            </Button>
+            <CartSheet />
          </Container>
       </div>
    )
