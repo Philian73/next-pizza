d9d655d2ca19a8f57449c1d87c1e23f2050dd8bc
Nikolay Sulteev <Philian73@gmail.com>
2025-10-23 20:45:12 +0300

    feat: add PATCH cart/cart-item endpoint

---
+++ app/api/cart/route.ts
@@ -0,0 +1,70 @@
+import type { Prisma } from '@prisma/client'
+import type { NextRequest } from 'next/server'
+
+import { NextResponse } from 'next/server'
+
+import { prisma } from '@/shared/lib/prisma'
+
+const emptyCart = NextResponse.json({ totalAmount: 0, items: [] })
+
+export async function GET(request: NextRequest) {
+   try {
+      const token = request.cookies.get('cart-token')?.value ?? ''
+
+      if (!token) {
+         return emptyCart
+      }
+
+      const userCart = await prisma.cart.findFirst({
+         where: { token },
+         select: {
+            totalAmount: true,
+            items: {
+               select: {
+                  id: true,
+                  variation: {
+                     select: {
+                        id: true,
+                        product: {
+                           select: {
+                              id: true,
+                              slug: true,
+                              name: true,
+                           },
+                        },
+                        imageUrl: true,
+                        price: true,
+                     },
+                  },
+                  quantity: true,
+                  toppings: true,
+                  removedIngredients: true,
+                  subtotal: true, // Добавляем выборку subtotal
+               },
+            },
+         },
+      })
+
+      if (!userCart) {
+         return emptyCart
+      }
+
+      return NextResponse.json({
+         totalAmount: userCart.totalAmount.toNumber(),
+         items: userCart.items.map(item => ({
+            id: item.id,
+            product: item.variation.product,
+            variationId: item.variation.id,
+            quantity: item.quantity,
+            imageUrl: item.variation.imageUrl,
+            toppings: item.toppings,
+            removedIngredients: item.removedIngredients,
+            subtotal: item.subtotal.toNumber(),
+         })),
+      })
+   } catch (error) {
+      console.error('Error fetching cart:', error)
+
+      return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
+   }
+}
